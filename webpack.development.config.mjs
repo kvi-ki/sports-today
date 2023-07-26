640afc50942a489426838368aa0df640cddee61a
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-07-26 18:49:50 +0200

    chore: add prettier configs

--- webpack.development.config.mjs
+++ webpack.development.config.mjs
@@ -5,29 +5,33 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const Config = {
-    mode: 'development',
-    entry: './src/index.tsx',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].[contenthash].js',
-        clean: true
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.mjs', '.json']
-    },
-    module: {
-        rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-            { test: /\.(js|jsx|tsx|ts)$/, exclude: /node_modules/, loader: 'babel-loader' },
-            { test: /\.(png|svg|jpe?g|gif)$/, use: [{ loader: 'file-loader' }] }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Deportes Hoy',
-            filename: 'index.html'
-        })
-    ]
-}
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.mjs', '.json'],
+  },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      { test: /\.(png|svg|jpe?g|gif)$/, use: [{ loader: 'file-loader' }] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Deportes Hoy',
+      filename: 'index.html',
+    }),
+  ],
+};
 
 export default Config;
