ea6f57cdea7762d177f8669b35fff96fe9389ef9
Victoria Kovaleva <victoriak.dev@gmail.com>
2023-08-09 12:43:39 +0200

    chore: update tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx,html,css}', './src/**/**/*.{js,jsx,ts,tsx,html,css}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    './src/**/**/*.{js,jsx,ts,tsx,html}'
+  ],
   theme: {
     extend: {}
   },
